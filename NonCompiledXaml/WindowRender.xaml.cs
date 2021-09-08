00045c57b993dfc02bd9e6bbe03443df75007f42
MrWhitebare <1766913528@qq.com>
2021-09-08 20:16:57 +0800

    新增布局面板

---
+++ NonCompiledXaml/WindowRender.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+using System.Windows.Markup;
+
+namespace NonCompiledXaml
+{
+    /// <summary>
+    /// WindowRender.xaml 的交互逻辑
+    /// </summary>
+    public partial class WindowRender : Window
+    {
+        public WindowRender()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent(string xamlFile)
+        {
+            //初始化窗体
+            this.Width = this.Height = 285;
+            this.Left = this.Top = 100;
+            this.Title = "自动加载 XAML文件";
+
+            DependencyObject rootElement;
+
+            using (FileStream fs = new FileStream(xamlFile, FileMode.Open))
+            {
+                rootElement = (DependencyObject) XamlReader.Load(fs);
+            }
+
+            this.Content = rootElement;
+            FrameworkElement frameworkElement = (FrameworkElement)rootElement;
+            btnChange = (Button)frameworkElement.FindName("btnChange");
+
+            btnChange.Click += new RoutedEventHandler(BtnChange_Click);
+
+        }
+
+        private void BtnChange_Click(object sender, RoutedEventArgs e)
+        {
+            btnChange.Content = "Thank you.";
+        }
+    }
+}
